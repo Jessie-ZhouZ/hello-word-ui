f76aeb8472fac83ef04acfb033f3473fa88b8e0f
Yanjie Zhou <yanjie.zhou@Yanjies-MacBook-Pro.local>
2022-04-15 11:49:49 +0800

    finish login

--- src/components/LoginIn.vue
+++ src/components/LoginIn.vue
@@ -44,43 +44,46 @@ export default {
     return {
       // 登录表单的数据绑定对象
       loginForm: {
-        username: "",
-        password: "",
+        username: 'admin',
+        password: '123456',
       },
       // 表单的验证规则对象
       loginFormRules: {
         username: [
-          { required: true, message: "请输入登录名称", trigger: "blur" },
-          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' },
         ],
         password: [
-          { required: true, message: "请输入登录密码", trigger: "blur" },
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
           {
             min: 6,
             max: 15,
-            message: "长度在 6 到 15 个字符",
-            trigger: "blur",
+            message: '长度在 6 到 15 个字符',
+            trigger: 'blur',
           },
         ],
       },
-    };
+    }
   },
   methods: {
     // 重置登录表单方法
     resetLoginForm() {
       // console.log(this);
-      this.$refs.loginFormRef.resetFields();
+      this.$refs.loginFormRef.resetFields()
     },
     login() {
       this.$refs.loginFormRef.validate(async (valid) => {
-        if (!valid) return;
-        const { data: res } = await this.$http.post("login", this.loginForm);
-        if (res.meta.status !== 200) return this.$message.error("登录失败");
-        return this.$message.success("登录成功");
-      });
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登录失败')
+        // console.log(res);
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
+        return this.$message.success('登录成功')
+      })
     },
   },
-};
+}
 </script>
 
 <style lang="scss" scoped>
--- src/router.js
+++ src/router.js
@@ -1,12 +1,27 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import LoginIn from './components/LoginIn.vue';
+import HomePage from './components/HomePage.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: LoginIn },
+    { path: '/home', component: HomePage },
   ],
 });
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从那个路径跳转而来
+  // next 是一个函数，表示放行
+  if (to.path === '/login') return next();
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token');
+  if (!tokenStr) return next('/login');
+  next();
+});
+export default router;
